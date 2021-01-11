d60c44f077b99fae656de3b8a3d97273e43a3a70
victorlomi <victorlominyo@gmail.com>
2021-01-11 16:44:49 +0300

    add photos to installed apps and add homepage template

--- photos/views.py
+++ photos/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Welcome to the photos gallery")
+	return render(request, 'homepage.html')
