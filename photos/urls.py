11201b0e2fca88eb886448e03400e576d12e4f44
victorlomi <victorlominyo@gmail.com>
2021-01-11 23:06:07 +0300

    add route for showing specific photos

--- photos/urls.py
+++ photos/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('photos/', views.photo, name="photos")
 ]
--- photos/views.py
+++ photos/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
+	"""Render a homepage with a grid of images"""
 	return render(request, 'homepage.html')
+
+def photo(request):
+	"""Render a photo with a specific image"""
+	return HttpResponse("showing a photo")
