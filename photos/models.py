4fbbb15778106fb09440f69330a42b49c686a776
victorlomi <victorlominyo@gmail.com>
2021-01-12 06:47:33 +0300

    add images to database and display on homepage

--- photos/models.py
+++ photos/models.py
@@ -2,9 +2,15 @@ from django.db import models
 
 class Location(models.Model):
 	name = models.CharField(max_length=60)
+
+	def __str__(self):
+		return self.name
     
 class Category(models.Model):
 	name = models.CharField(max_length=60)
+
+	def __str__(self):
+		return self.name
 
 class Image(models.Model):
 	image = models.ImageField(upload_to="photos/")
@@ -12,3 +18,6 @@ class Image(models.Model):
 	description = models.TextField()
 	location = models.ForeignKey(Location, on_delete=models.CASCADE)
 	category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.name
--- photos/views.py
+++ photos/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Image
+
 def index(request):
 	"""Render a homepage with a grid of images"""
-	return render(request, 'homepage.html')
+	photos = Image.objects.all()
+	return render(request, 'homepage.html', {"photos": photos})
 
 def photo(request, photo_id):
 	"""Render a photo with a specific image"""
