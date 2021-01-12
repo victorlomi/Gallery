d8f58645b43970589a456aff179350f2e6faa727
victorlomi <victorlominyo@gmail.com>
2021-01-12 09:37:46 +0300

    add gunicorn and Procfile for deployment

--- Gallery/settings.py
+++ Gallery/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&slj+o+n+axx6l87(hooenjh9i6*wc80xq@_eu1^m0l=4nx+2j'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
