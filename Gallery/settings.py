e282ec120c7dfc34bac24567912116d95d98bf2f
victorlomi <victorlominyo@gmail.com>
2021-01-12 06:13:11 +0300

    remove unnessarcy images from folder

--- Gallery/settings.py
+++ Gallery/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -77,8 +78,10 @@ WSGI_APPLICATION = 'Gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'photos',
+        'USER': 'plutos28',
+        'PASSWORD':'julia2005',
     }
 }
 
@@ -120,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Location(models.Model):
+	name = models.CharField(max_length=60)
+    
+class Category(models.Model):
+	name = models.CharField(max_length=60)
+
+class Image(models.Model):
+	image = models.ImageField(upload_to="photos/")
+	name = models.CharField(max_length=60)
+	description = models.TextField()
+	location = models.ForeignKey(Location, on_delete=models.CASCADE)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
